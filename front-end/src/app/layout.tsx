9824875cdf2d471aa1ae63393e07298d449bc9ff
alikansoh <alikanso.dev@gmail.com>
2025-08-04 18:48:56 +0100

    add a hero section

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
 
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased mt-[168px]`}
       >
         <Navbar/>
         {children}
